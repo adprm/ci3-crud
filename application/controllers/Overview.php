d7330d10c6a8594f06da0ee37df243ef5b667880
adprm <aditiyaprmn00@gmail.com>
2020-08-13 11:57:03 +0700

    load view

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -5,7 +5,12 @@ class Overview extends CI_Controller {
 
     public function index() {
         $data['title'] = "CI-CRUD";
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('templates/sidebar');
         $this->load->view('admin/overview', $data);
+        $this->load->view('templates/footer');
     }
 
 }
