fee002418f4cb91dad368c9537119cdf50476cb5
adprm <aditiyaprmn00@gmail.com>
2020-08-13 12:58:35 +0700

    modified view dashboard

--- application/views/admin/overview.php
+++ application/views/admin/overview.php
@@ -1,8 +1,7 @@
-<!-- content -->
 <div id="layoutSidenav_content">
     <main>
         <div class="container-fluid">
             <h1 class="mt-4">Dashboard</h1>
         </div>
-</main>
-<!-- /end content -->
+    </main>
+                
