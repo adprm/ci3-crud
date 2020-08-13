50dd64b7fd89c2e18c73f93b582f51ff6b233547
adprm <aditiyaprmn00@gmail.com>
2020-08-13 13:42:25 +0700

    load db

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -2,5 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Student_model extends CI_Model {
+    private $_table = 'mahasiswa';
 
+    public $id;
+    public $name;
+    public $nim;
+    public $email;
+    public $image;
+    
 }
