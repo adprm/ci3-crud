d61e91eb45dd867839b8029d587dd79689a93d24
adprm <aditiyaprmn00@gmail.com>
2020-08-13 13:37:17 +0700

    render load view

--- application/controllers/Students.php
+++ application/controllers/Students.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Students extends CI_Controller {
 
-    
+    public function index() {
+        $data['title'] = "Students";
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('templates/sidebar');
+        $this->load->view('student/index', $data);
+        $this->load->view('templates/footer');
+    }
 
 }
