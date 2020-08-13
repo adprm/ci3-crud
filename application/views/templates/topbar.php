88f2fa1409925beabfd3a93bffe16346f33e9e2f
adprm <aditiyaprmn00@gmail.com>
2020-08-13 12:59:50 +0700

    modified topbar

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -1,6 +1,5 @@
-<!-- navbar -->
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.html"><?= $title ?></a>
-    <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-</nav>
-<!-- / end navbar -->
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.html"><?= $title; ?></a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
