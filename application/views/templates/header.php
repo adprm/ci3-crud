c6b9a7483109db7d4b49973f6955b72eb667f601
adprm <aditiyaprmn00@gmail.com>
2020-08-13 12:59:16 +0700

    modified header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,4 +11,3 @@
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
     </head>
-<body class="sb-nav-fixed">
