29573f2ba35e90b1327e4e0c50ec1127109f0069
adprm <aditiyaprmn00@gmail.com>
2020-08-13 13:16:09 +0700

    set title

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -1,5 +1,5 @@
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.html"><?= $title; ?></a>
+        <a class="navbar-brand" href="index.html">CI-CRUD</a>
         <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
     </nav>
