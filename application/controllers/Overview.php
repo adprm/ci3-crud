2be3358a11f07c3bbb592903f8ff7e0c403b44a3
adprm <aditiyaprmn00@gmail.com>
2020-08-13 10:48:54 +0700

    data title

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -3,4 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Overview extends CI_Controller {
 
+    public function index() {
+        $data['title'] = "CI-CRUD";
+        $this->load->view('admin/overview');
+    }
+
 }
