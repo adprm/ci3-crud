5514e77d2ad8f0a6793885c17b0f95f46e432d95
adprm <aditiyaprmn00@gmail.com>
2020-08-13 11:57:55 +0700

    footer

---
+++ application/views/templates/footer.php
@@ -0,0 +1,30 @@
+<!-- footer -->
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; ADPRM <?= date('Y'); ?></div>
+        <div>
+            <a href="#" class="text-muted"><i class="fab fa-facebook"></i> Facebook </a>
+            &middot;
+            <a href="#" class="text-muted"><i class="fab fa-linkedin"></i> Linkedin </a>
+            &middot;
+            <a href="#" class="text-muted"><i class="fab fa-github"></i> Github </a>
+        </div>
+    </div>
+    </div>
+</footer>
+<!-- /end footer -->
+</div>
+<!-- /end content -->
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url('assets/'); ?>js/scripts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url('assets/'); ?>assets/demo/chart-area-demo.js"></script>
+<script src="<?= base_url('assets/'); ?>assets/demo/chart-bar-demo.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url('assets/'); ?>assets/demo/datatables-demo.js"></script>
+</body>
+</html>
