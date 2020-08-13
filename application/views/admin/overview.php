078d0642d53ae05c786c482701d8f91ada4441b4
adprm <aditiyaprmn00@gmail.com>
2020-08-13 13:15:39 +0700

    set title

--- application/views/admin/overview.php
+++ application/views/admin/overview.php
@@ -1,7 +1,7 @@
 <div id="layoutSidenav_content">
     <main>
         <div class="container-fluid">
-            <h1 class="mt-4">Dashboard</h1>
+            <h1 class="mt-4"><?= $title; ?></h1>
         </div>
     </main>
                 
