d7cee93163878937501bb14e3f5750384fb68b71
adprm <aditiyaprmn00@gmail.com>
2020-08-14 13:35:53 +0700

    bugfix method edit data

--- application/controllers/Students.php
+++ application/controllers/Students.php
@@ -34,11 +34,11 @@ class Students extends CI_Controller {
             $this->load->view('templates/sidebar');
             $this->load->view('student/add_data', $data);
             $this->load->view('templates/footer');
-            $this->session->set_flashdata('message_error_addstudent', '<div class="alert alert-danger" role="alert">
-            New student dont added!</div>');
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            New student failed to added!</div>');
         } else {
             $student->save();
-            $this->session->set_flashdata('message_success_addstudent', '<div class="alert alert-success" role="alert">
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
             New student added!</div>');
             redirect('students');
         }
@@ -46,23 +46,35 @@ class Students extends CI_Controller {
 
     public function edit($id = null) {
         $data['title'] = "Edit Data Student";
-        
-        if (!isset($id)) redirect('students');
-
         $student = $this->student_model;
         $data['student'] = $student->getById($id);
 
+        if (!isset($id)) redirect('students');
+
+        
+        $data['student'] = $student->getById($id);
+        if (!$data['student']) show_404();
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('templates/sidebar');
+        $this->load->view('student/edit_data', $data);
+        $this->load->view('templates/footer');
+        
         $this->form_validation->set_rules('name', 'Name', 'required');
         $this->form_validation->set_rules('nim', 'Nim', 'required|min_length[8]');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
 
         if ($this->form_validation->run() == false) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('templates/sidebar');
-            $this->load->view('student/edit_data', $data);
-            $this->load->view('templates/footer');
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Data failed to edit!</div>');
+        } else {
+            $student->update();
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Data edited successfully!</div>');
+            redirect('students');
         }
+
     }
 
 }
