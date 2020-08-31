8d748ea13422d2c203c9d5f9337d38ca73c3a71e
adprm <aditiyaprmn00@gmail.com>
2020-08-31 18:30:14 +0700

    created data title

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -5,10 +5,12 @@ class Overview extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('templates/header');
+        $data['title'] = 'Dashboard';
+
+        $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar');
         $this->load->view('templates/topbar');
-        $this->load->view('overview/index');
+        $this->load->view('overview/index', $data);
         $this->load->view('templates/footer');
     }
 
