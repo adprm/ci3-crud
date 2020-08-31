4df2ff58afa5e9ddc4f93b069a4899b07b66b7ef
adprm <aditiyaprmn00@gmail.com>
2020-08-31 17:38:16 +0700

    controller view

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Overview extends CI_Controller {
 
-
+    public function index()
+    {
+        $this->load->view('overview/index');
+    }
 
 }
