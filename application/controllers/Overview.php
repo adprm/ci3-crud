9bfa924548222dadcd26a255cdb3761d42a88ecc
adprm <aditiyaprmn00@gmail.com>
2020-08-31 18:27:57 +0700

    load templates

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -5,7 +5,11 @@ class Overview extends CI_Controller {
 
     public function index()
     {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
         $this->load->view('overview/index');
+        $this->load->view('templates/footer');
     }
 
 }
