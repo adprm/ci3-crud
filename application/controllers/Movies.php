d4f659fe9e1d17cde9e5978a81c2a26662a246b8
adprm <aditiyaprmn00@gmail.com>
2020-08-31 19:28:27 +0700

    created movies controller

---
+++ application/controllers/Movies.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Movies extends CI_Controller {
+
+    public function index()
+    {
+        $data['title'] = 'List Movies';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('movie/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+}
