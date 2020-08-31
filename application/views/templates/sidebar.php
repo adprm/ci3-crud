8bf0703e53c867c4ce7cb91ed0fbeb2515630804
adprm <aditiyaprmn00@gmail.com>
2020-08-31 19:18:28 +0700

    modified sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -25,20 +25,20 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Interface
+        Data Management
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+            <span>Movies</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
+                <h6 class="collapse-header">Movies</h6>
+                <a class="collapse-item" href="buttons.html">List Movies</a>
+                <a class="collapse-item" href="cards.html">Add Movie</a>
             </div>
         </div>
     </li>
