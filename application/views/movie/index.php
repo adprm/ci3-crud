271c6744df2dc485260766aa9eda85cc70290e8d
adprm <aditiyaprmn00@gmail.com>
2020-09-01 10:15:22 +0700

    add session message

--- application/views/movie/index.php
+++ application/views/movie/index.php
@@ -4,10 +4,14 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+    <div class="col-lg-4">
+        <?= $this->session->flashdata('message'); ?>
+    </div> 
+
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            <h6 class="m-0 font-weight-bold text-primary"><a href="<?php echo site_url('movies/add/') ?>"><i class="fas fa-plus"></i> Add Movie</a></h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
