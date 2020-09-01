2d7c7ec5fdeda5a9965250fe0cff0bf9bea8b849
adprm <aditiyaprmn00@gmail.com>
2020-09-01 09:32:17 +0700

    active link

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -29,9 +29,9 @@
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
+    <li class="nav-item <?php if($this->uri->segment(1)=="movies"){echo "active";} ?>">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
+            <i class="fas fa-fw fa-film"></i>
             <span>Movies</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
