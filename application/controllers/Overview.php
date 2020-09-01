886c1a86525c5fcb9b3a51ee98cc7cf06b318fa4
adprm <aditiyaprmn00@gmail.com>
2020-09-01 13:37:50 +0700

    view total data

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -3,9 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Overview extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Movie_model');
+    }
+
     public function index()
     {
         $data['title'] = 'Dashboard';
+        $data['totalMovie'] = $this->Movie_model->totalMovie();
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar');
