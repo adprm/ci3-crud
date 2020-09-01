c40421789e3bb0cea3ec71ef4234dc6c4e87aeae
adprm <aditiyaprmn00@gmail.com>
2020-09-01 10:14:21 +0700

    method upload image

--- application/models/Movie_model.php
+++ application/models/Movie_model.php
@@ -17,15 +17,32 @@ Class Movie_model extends CI_Model {
     // method save
     public function save()
     {
-        $data = [
-            'id' => uniqid(),
-            'title' => $this->input->post('title', true),
-            'desc' => $this->input->post('desc', true),
-            'release' => $this->input->post('release', true),
-            'poster' => $this->_uploadImage()
-        ];
+        $post = $this->input->post();
+        $this->id = uniqid();
+        $this->title = $this->input->post('title');
+        $this->desc = $this->input->post('desc');
+        $this->release = $this->input->post('release');
+        $this->poster = $this->_uploadImage();
 
-        return $this->db->insert('movies', $data);
+        return $this->db->insert('movies', $this);
+    }
+
+    // method upload image
+    private function _uploadImage()
+    {
+        $config['upload_path']      = './assets/img/';
+        $config['allowed_types']    = 'jpg|png|jpeg';
+        $config['file_name']        = $this->id;
+        $config['overwrite']        = true;
+        $config['max_size']         = '15000';
+
+        $this->load->library('upload', $config);
+
+        if ($this->upload->do_upload('poster')) {
+            return $this->upload->data('file_name');
+        }
+
+        return "default.jpg";
     }
 
 }
