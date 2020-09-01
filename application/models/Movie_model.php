455b33b91bba29f4afd345502c3b05ec7e10d559
adprm <aditiyaprmn00@gmail.com>
2020-09-01 09:45:54 +0700

    method save

--- application/models/Movie_model.php
+++ application/models/Movie_model.php
@@ -14,4 +14,18 @@ Class Movie_model extends CI_Model {
         return $this->db->get_where('movies', ['id' => $id])->row();
     }
 
+    // method save
+    public function save()
+    {
+        $data = [
+            'id' => uniqid(),
+            'title' => $this->input->post('title', true),
+            'desc' => $this->input->post('desc', true),
+            'release' => $this->input->post('release', true),
+            'poster' => $this->_uploadImage()
+        ];
+
+        return $this->db->insert('movies', $data);
+    }
+
 }
