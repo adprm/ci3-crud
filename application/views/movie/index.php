d20d390dd3067d59b41e8a02249d54d04ab87695
adprm <aditiyaprmn00@gmail.com>
2020-09-01 13:38:10 +0700

    bugfix

--- application/views/movie/index.php
+++ application/views/movie/index.php
@@ -41,7 +41,7 @@
                         <tr>
                             <td><?= $index; ?></td>
                             <td><?= $m->title; ?></td>
-                            <td><?= $m->desc; ?></td>
+                            <td class="small"><?= substr($m->desc, 0, 80); ?>...</td>
                             <td><?= $m->release; ?></td>
                             <td>
                                 <img src="<?php echo base_url('assets/img/'.$m->poster) ?>" width="64" />
@@ -69,7 +69,7 @@
 <!-- modal delete -->
 <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content border-left-danger">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
         <button class="close" type="button" data-dismiss="modal" aria-label="Close">
